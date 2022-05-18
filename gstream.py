25993a25e15992ad8ebc2136c989d44b5e5350b0
kst_obd <kitcat3903@gmail.com>
2022-05-18 11:14:20 +0300

    first commit

---
+++ gstream.py
@@ -0,0 +1,26 @@
+from threading import Thread
+
+import gi
+
+gi.require_version("Gst", "1.0")
+
+from gi.repository import Gst, GLib
+
+
+Gst.init()
+
+main_loop = GLib.MainLoop()
+thread = Thread(target=main_loop.run)
+thread.start()
+
+pipeline = Gst.parse_launch("autovideosrc ! decodebin ! videoconvert ! autovideosink")
+pipeline.set_state(Gst.State.PLAYING)
+
+try:
+    while True:
+        sleep(0.1)
+except KeyboardInterrupt:
+    pass
+
+pipeline.set_state(Gst.State.NULL)
+main_loop.quit()
