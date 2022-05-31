a1cf87cd45abdb8eca71d1d8c7c4c9d20d076665
kstobd <kitcat3903@gmail.com>
2022-05-31 15:15:20 +0300

    working stream, but not optimized

--- start.py
+++ start.py
@@ -9,8 +9,13 @@ Created on Mon Jan  20 02:07:13 2019
 # from main.py import frame
 
 # import necessary argumnets 
-import gi
+import face_recognition
+import imutils
+import pickle
+import time
 import cv2
+import os
+import gi
 import argparse
 
 # import required library like Gstreamer and GstreamerRtspServer
@@ -19,6 +24,65 @@ gi.require_version('GstRtspServer', '1.0')
 from gi.repository import Gst, GstRtspServer, GObject, GLib
 # Sensor Factory class which inherits the GstRtspServer base class and add
 # properties to it.
+
+def make_frame(video_capture):
+    # ret, frame = video_capture.read()
+    frame = video_capture
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray,
+                                         scaleFactor=1.1,
+                                         minNeighbors=5,
+                                         minSize=(60, 60),
+                                         flags=cv2.CASCADE_SCALE_IMAGE)
+
+    # convert the input frame from BGR to RGB
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # the facial embeddings for face in input
+    encodings = face_recognition.face_encodings(rgb)
+    names = []
+    # loop over the facial embeddings incase
+    # we have multiple embeddings for multiple fcaes
+    for encoding in encodings:
+        # Compare encodings with encodings in data["encodings"]
+        # Matches contain array with boolean values and True for the embeddings it matches closely
+        # and False for rest
+        matches = face_recognition.compare_faces(data["encodings"],
+                                                 encoding)
+        # set name =inknown if no encoding matches
+        name = "Unknown"
+        # check to see if we have found a match
+        if True in matches:
+            # Find positions at which we get True and store them
+            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
+            # loop over the matched indexes and maintain a count for
+            # each recognized face face
+            for i in matchedIdxs:
+                # Check the names at respective indexes we stored in matchedIdxs
+                name = data["names"][i]
+                # increase count for the name we got
+                counts[name] = counts.get(name, 0) + 1
+            # set name which has highest count
+            name = max(counts, key=counts.get)
+
+        # update the list of names
+        names.append(name)
+        # loop over the recognized faces
+        for ((x, y, w, h), name) in zip(faces, names):
+            # rescale the face coordinates
+            # draw the predicted face name on the image
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.75, (0, 255, 0), 2)
+    return frame
+                        
+cascPathface = os.path.dirname(
+    cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml"
+# load the harcaascade in the cascade classifier
+faceCascade = cv2.CascadeClassifier(cascPathface)
+# load the known faces and embeddings saved in last file
+data = pickle.loads(open('face_enc', "rb").read())
+
 class SensorFactory(GstRtspServer.RTSPMediaFactory):
     def __init__(self, **properties):
         super(SensorFactory, self).__init__(**properties)
@@ -41,8 +105,9 @@ class SensorFactory(GstRtspServer.RTSPMediaFactory):
             if ret:
                 # It is better to change the resolution of the camera 
                 # instead of changing the image shape as it affects the image quality.
-                frame = cv2.resize(frame, (opt.image_width, opt.image_height), \
-                    interpolation = cv2.INTER_LINEAR)
+                frame = make_frame(frame)
+                # frame = cv2.resize(frame, (opt.image_width, opt.image_height), \
+                    # interpolation = cv2.INTER_LINEAR)
                 data = frame.tostring()
                 buf = Gst.Buffer.new_allocate(None, len(data), None)
                 buf.fill(0, data)
